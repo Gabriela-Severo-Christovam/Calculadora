22daff33218448d3ab9bd1ba752acd15a03c04e9
Gabriela Severo <severogabriela4@gmail.com>
2024-09-17 14:44:30 -0300

    Alterando design da calculadora

--- jogo_calc.Designer.cs
+++ jogo_calc.Designer.cs
@@ -57,6 +57,7 @@
             // 
             // mais
             // 
+            mais.BackColor = Color.LightCoral;
             mais.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             mais.Location = new Point(275, 131);
             mais.Name = "mais";
@@ -64,10 +65,11 @@
             mais.TabIndex = 1;
             mais.Text = "+";
             mais.TextAlign = ContentAlignment.TopCenter;
-            mais.UseVisualStyleBackColor = true;
+            mais.UseVisualStyleBackColor = false;
             // 
             // menos
             // 
+            menos.BackColor = Color.LightCoral;
             menos.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             menos.Location = new Point(326, 131);
             menos.Name = "menos";
@@ -75,10 +77,11 @@
             menos.TabIndex = 15;
             menos.Text = "-";
             menos.TextAlign = ContentAlignment.TopCenter;
-            menos.UseVisualStyleBackColor = true;
+            menos.UseVisualStyleBackColor = false;
             // 
             // vezes
             // 
+            vezes.BackColor = Color.LightCoral;
             vezes.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             vezes.Location = new Point(378, 131);
             vezes.Name = "vezes";
@@ -86,10 +89,11 @@
             vezes.TabIndex = 16;
             vezes.Text = "*";
             vezes.TextAlign = ContentAlignment.TopCenter;
-            vezes.UseVisualStyleBackColor = true;
+            vezes.UseVisualStyleBackColor = false;
             // 
             // divisao
             // 
+            divisao.BackColor = Color.LightCoral;
             divisao.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             divisao.Location = new Point(430, 131);
             divisao.Name = "divisao";
@@ -97,132 +101,145 @@
             divisao.TabIndex = 17;
             divisao.Text = "/";
             divisao.TextAlign = ContentAlignment.TopCenter;
-            divisao.UseVisualStyleBackColor = true;
+            divisao.UseVisualStyleBackColor = false;
             // 
             // apaga
             // 
+            apaga.BackColor = Color.LightCoral;
             apaga.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             apaga.Location = new Point(430, 181);
             apaga.Name = "apaga";
             apaga.Size = new Size(46, 45);
             apaga.TabIndex = 18;
             apaga.Text = "CLS";
-            apaga.UseVisualStyleBackColor = true;
+            apaga.UseVisualStyleBackColor = false;
             // 
             // tres
             // 
+            tres.BackColor = Color.LightCoral;
             tres.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             tres.Location = new Point(378, 181);
             tres.Name = "tres";
             tres.Size = new Size(46, 45);
             tres.TabIndex = 19;
             tres.Text = "3";
-            tres.UseVisualStyleBackColor = true;
+            tres.UseVisualStyleBackColor = false;
             // 
             // dois
             // 
+            dois.BackColor = Color.LightCoral;
             dois.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             dois.Location = new Point(327, 181);
             dois.Name = "dois";
             dois.Size = new Size(46, 45);
             dois.TabIndex = 20;
             dois.Text = "2";
-            dois.UseVisualStyleBackColor = true;
+            dois.UseVisualStyleBackColor = false;
             // 
             // um
             // 
+            um.BackColor = Color.LightCoral;
             um.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             um.Location = new Point(275, 181);
             um.Name = "um";
             um.Size = new Size(46, 45);
             um.TabIndex = 21;
             um.Text = "1";
-            um.UseVisualStyleBackColor = true;
+            um.UseVisualStyleBackColor = false;
             // 
             // igual
             // 
+            igual.BackColor = Color.LightCoral;
             igual.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             igual.Location = new Point(430, 231);
             igual.Name = "igual";
             igual.Size = new Size(46, 141);
             igual.TabIndex = 22;
             igual.Text = "=";
-            igual.UseVisualStyleBackColor = true;
+            igual.UseVisualStyleBackColor = false;
             // 
             // seis
             // 
+            seis.BackColor = Color.LightCoral;
             seis.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             seis.Location = new Point(379, 231);
             seis.Name = "seis";
             seis.Size = new Size(46, 45);
             seis.TabIndex = 23;
             seis.Text = "6";
-            seis.UseVisualStyleBackColor = true;
+            seis.UseVisualStyleBackColor = false;
             // 
             // cinco
             // 
+            cinco.BackColor = Color.LightCoral;
             cinco.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             cinco.Location = new Point(327, 231);
             cinco.Name = "cinco";
             cinco.Size = new Size(46, 45);
             cinco.TabIndex = 24;
             cinco.Text = "5";
-            cinco.UseVisualStyleBackColor = true;
+            cinco.UseVisualStyleBackColor = false;
             // 
             // quatro
             // 
+            quatro.BackColor = Color.LightCoral;
             quatro.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             quatro.Location = new Point(275, 231);
             quatro.Name = "quatro";
             quatro.Size = new Size(46, 45);
             quatro.TabIndex = 25;
             quatro.Text = "4";
-            quatro.UseVisualStyleBackColor = true;
+            quatro.UseVisualStyleBackColor = false;
             // 
             // nove
             // 
+            nove.BackColor = Color.LightCoral;
             nove.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             nove.Location = new Point(379, 282);
             nove.Name = "nove";
             nove.Size = new Size(46, 45);
             nove.TabIndex = 26;
             nove.Text = "9";
-            nove.UseVisualStyleBackColor = true;
+            nove.UseVisualStyleBackColor = false;
             // 
             // oito
             // 
+            oito.BackColor = Color.LightCoral;
             oito.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             oito.Location = new Point(327, 282);
             oito.Name = "oito";
             oito.Size = new Size(46, 45);
             oito.TabIndex = 27;
             oito.Text = "8";
-            oito.UseVisualStyleBackColor = true;
+            oito.UseVisualStyleBackColor = false;
             // 
             // sete
             // 
+            sete.BackColor = Color.LightCoral;
             sete.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             sete.Location = new Point(275, 282);
             sete.Name = "sete";
             sete.Size = new Size(46, 45);
             sete.TabIndex = 28;
             sete.Text = "7";
-            sete.UseVisualStyleBackColor = true;
+            sete.UseVisualStyleBackColor = false;
             // 
             // zero
             // 
+            zero.BackColor = Color.LightCoral;
             zero.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             zero.Location = new Point(275, 333);
             zero.Name = "zero";
-            zero.Size = new Size(149, 39);
+            zero.Size = new Size(150, 39);
             zero.TabIndex = 29;
             zero.Text = "0";
-            zero.UseVisualStyleBackColor = true;
+            zero.UseVisualStyleBackColor = false;
             // 
             // jogo_calc
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MistyRose;
             ClientSize = new Size(800, 450);
             Controls.Add(zero);
             Controls.Add(sete);
