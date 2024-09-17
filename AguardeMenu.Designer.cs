37ce319dd6ab790cc86923122c6f8c6870dc7d5a
Gabriela Severo <severogabriela4@gmail.com>
2024-09-17 09:56:55 -0300

    MODIFY: Alterando a posição inicial da tela  sua borda

--- AguardeMenu.Designer.cs
+++ AguardeMenu.Designer.cs
@@ -81,6 +81,7 @@
             Controls.Add(pictureBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Calculadora";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
